40b84558537590043497b79c2dae67bdf50ab4ee
Benoit MORTGAT <mortgatbenoit@gmail.com>
2017-03-16 10:55:25 +0100

    add board very large

--- src/game.py
+++ src/game.py
@@ -41,23 +41,53 @@ if __name__ == '__main__':
                    {'x': 3, 'y': 1, HUM: 3, VAMP: 0, WOLV: 0},
                    {'x': 4, 'y': 3, HUM: 0, VAMP: 0, WOLV: 3}]
 
+    initial_pop2 = [{'x': 0, 'y': 0, HUM: 0, VAMP: 0, WOLV: 5},
+                    {'x': 1, 'y': 1, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 1, 'y': 3, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 2, 'y': 2, HUM: 9, VAMP: 0, WOLV: 0},
+                    {'x': 1, 'y': 1, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 3, 'y': 1, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 3, 'y': 3, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 3, 'y': 5, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 4, 'y': 5, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 5, 'y': 3, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 5, 'y': 4, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 5, 'y': 6, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 5, 'y': 7, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 6, 'y': 1, HUM: 5, VAMP: 0, WOLV: 0},
+                    {'x': 6, 'y': 5, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 7, 'y': 3, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 7, 'y': 5, HUM: 2, VAMP: 0, WOLV: 0},
+                    {'x': 7, 'y': 7, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 7, 'y': 9, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 8, 'y': 2, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 8, 'y': 8, HUM: 9, VAMP: 0, WOLV: 0},
+                    {'x': 9, 'y': 1, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 9, 'y': 4, HUM: 5, VAMP: 0, WOLV: 0},
+                    {'x': 9, 'y': 7, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 9, 'y': 9, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 10, 'y': 0, HUM: 1, VAMP: 0, WOLV: 0},
+                    {'x': 10, 'y': 10, HUM: 0, VAMP: 5, WOLV: 0},
+                    ]
+
     board = Board((4, 5), initial_pop)
+    board2 = Board((11, 11), initial_pop2)
 
     #player1 = MapPlayer(VAMP, depth=5)
     #player1 = MapPlayer(VAMP, depth=7, type='alphabeta')
-    player1 = SmartPlayerAlpha(VAMP, depth=7)
+    player1 = SmartPlayerAlpha(VAMP, depth=3)
 
     #player2 = MapPlayer(WOLV, depth=5)
     #player2 = MapPlayer(WOLV, depth=7, type='alphabeta')
-    player2 = SmartPlayerAlpha(WOLV, depth=7)
+    player2 = SmartPlayerAlpha(WOLV, depth=3)
     #player2 = RandomPlayer(WOLV)
 
     GUI = True
     if GUI:
         from draw import start_GUI, draw
-        start_GUI(board.grid, generate_play(player1, player2, board, draw))
+        start_GUI(board2.grid, generate_play(player1, player2, board2, draw))
     else:
-        play = generate_play(player1, player2, board)
+        play = generate_play(player1, player2, board2)
         play()
         #import cProfile
         # cProfile.run('play()')
